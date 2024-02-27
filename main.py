5b7e6382aa6251a4abf7adeecdbea6a025b9bd87
yuhsi chen <38905980+wish44165@users.noreply.github.com>
2024-02-27 19:12:28 +1100

    Update main.py

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ def parse_opt(known=False):
     parser.add_argument('--yaml_path', type=str, default='./facial.yaml', help='The yaml path')
     parser.add_argument('--n_epoch', type=int, default=300, help='Total number of training epochs.')
     parser.add_argument('--n_patience', type=int, default=100, help='Number of epochs to wait without improvement in validation metrics before early stopping the training.')
-    parser.add_argument('--bs', type=int, default=32, help='Batch size')
+    parser.add_argument('--bs', type=int, default=48, help='Batch size')
     parser.add_argument('--imgsz', type=int, default=640, help='Image size')
     parser.add_argument('--n_worker', type=int, default=8, help='Number of workers')
     parser.add_argument('--save_path', type=str, default='./runs/facial/', help='Save path')
@@ -132,8 +132,8 @@ def parse_opt(known=False):
     # predict (will change)
     parser.add_argument('--curr_iter', type=int, default=0, help='current iteration')
     parser.add_argument('--predict_weight', type=str, default='', help='previous best weight')
-    parser.add_argument('--training_folder', type=str, default='../datasets/v0/', help='predict on training data')
-    parser.add_argument('--generate_folder', type=str, default='../datasets/', help='generate new training data structure')
+    parser.add_argument('--training_folder', type=str, default='../../datasets/v0/', help='predict on training data')
+    parser.add_argument('--generate_folder', type=str, default='../../datasets/', help='generate new training data structure')
     
     return parser.parse_known_args()[0] if known else parser.parse_args()
 
